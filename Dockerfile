1bd330718d63a1b9721db242aed9ae4d03598d50
gyanendra <gyanendras1898@gmail.com>
2023-02-19 22:22:49 +0530

    fifth commit

--- Dockerfile
+++
@@ -1,3 +0,0 @@
-FROM openjdk:8
-ADD ./target/docker-spring-boot.war docker-spring-boot.war
-ENTRYPOINT [ "java","war","docker-spring-boot.war" ]
--- src/main/java/com/gyan/client/Application.java
+++ src/main/java/com/gyan/client/Application.java
@@ -3,6 +3,8 @@ package com.gyan.client;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "com.gyan")
@@ -11,7 +13,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class Application {
 	
 	public String PORT = System.getenv("PORT");
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
